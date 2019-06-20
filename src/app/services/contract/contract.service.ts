306ab116088cc3efd447e1e0dfec47c858abf3ed
Antonio Cardenas <antonioalx66@gmail.com>
2019-06-19 18:19:44 -0600

    changes and deploy

--- src/app/services/contract/contract.service.ts
+++ src/app/services/contract/contract.service.ts
@@ -157,7 +157,7 @@ export class ContractService {
             hackathon_id,
             {
               from: originAccount,
-              value: window.web3.utils.toWei('0.03', 'ether')
+              value: window.web3.utils.toWei('0.02', 'ether')
             }
           );
         }).then((status) => {
